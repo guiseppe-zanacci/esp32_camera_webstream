9d6b8f16210072e3d6334e8ba67d3224b99e6373
Vtrixx <vidar.wicklund@hotmail.com>
2024-10-22 13:44:31 +0200

    Added PlatformIO structure

---
+++ esp32_stream_software/src/credentials.h
@@ -0,0 +1,13 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define TEST_SERVER
+//#define LIVE_SERVER
+
+extern const char* ssid; 
+extern const char* password;
+
+extern const char* websockets_server_host;
+extern const int websockets_server_port;
+
+#endif
